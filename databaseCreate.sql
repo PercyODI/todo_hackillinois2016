25ce8cb14853782d5cdc91487cb6f6fe08d5ce83
Pearse Hutson <pearse.2008@gmail.com>
2016-02-21 16:05:07 +0000

    database stuff

--- databaseCreate.sql
+++ databaseCreate.sql
@@ -3,12 +3,16 @@ set foreign_key_checks=0;
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
 	uid SERIAL,
-	usernamee VARCHAR(15),
+	username VARCHAR(15) UNIQUE NOT NULL,
 	password VARCHAR(255),
 	firstname VARCHAR(30),
 	lastname VARCHAR(30),
 	PRIMARY KEY(uid)
 )ENGINE=InnoDB;
+
+INSERT INTO user (username, password, firstname, lastname) VALUES
+	("pah9qd", "password", "Pearse", "Hutson"),
+	("hayleyWho", "123abc", "Hayley", "Hutson");
 
 
 DROP TABLE IF EXISTS list;
@@ -22,6 +26,10 @@ CREATE TABLE list (
 	FOREIGN KEY(uid) REFERENCES user(uid)
 )ENGINE=InnoDB;
 
+INSERT INTO list (uid, listname, datecreated, type) VALUES
+	((SELECT uid FROM user WHERE username = "pah9qd"), "App Ideas", NOW(), "Regular"),
+	((SELECT uid FROM user WHERE username = "hayleyWho"), "Things to Clean", NOW(), "OCD Plus");
+
 DROP TABLE IF EXISTS task;
 CREATE TABLE task (
 	tid SERIAL,
@@ -32,5 +40,11 @@ CREATE TABLE task (
 	PRIMARY KEY(tid),
 	FOREIGN KEY(lid) REFERENCES list(lid)
 )ENGINE=InnoDB;
+
+INSERT INTO task (lid, tasktext, datecreated, completed) VALUES
+	((SELECT lid FROM list WHERE listname = "Things to Clean"), "Kitchen Sink", NOW(), 0),
+	((SELECT lid FROM list WHERE listname = "Things to Clean"), "Jessie's Toilet", NOW(), 1),
+	((SELECT lid FROM list WHERE listname = "Things to Clean"), "Computer (Keyboard is disgusting!)", NOW(), 0),
+	((SELECT lid FROM list WHERE listname = "App Ideas"), "A To-Do application for websites", NOW(), 0);
 
 set foreign_key_checks=1;
