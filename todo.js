f5e30692090a934b5330ec1bfd1488b3857d0230
PercyODI <pearse.2008@gmail.com>
2016-02-21 10:03:33 -0600

    Getting ready for cloud9

--- todo.js
+++ todo.js
@@ -1,19 +1,35 @@
 function newTask(addBeforeElem) {
     var newTaskTextField;
+
+    function newTaskTextFieldFinished() {
+        //Remove TaskTextField
+        newTaskTextField.fadeOut("fast", function() {
+            if(newTaskTextField.val() != "") {
+                addBeforeElem.before("<li>" + newTaskTextField.val() + "</li>");
+            }
+            this.remove();
+        });
+    }
+
     console.log("A new task needs to be created!");
     addBeforeElem.before(
         "<li><input type='text' id='newTaskTextField' class='form-control' placeholder='Insert New Task'></input></li>");
     newTaskTextField = $("#newTaskTextField");
 
     newTaskTextField.focus();
+
+    //What to do on field deselected
     newTaskTextField.blur(function() {
-        if(newTaskTextField.val() == "") {
-            newTaskTextField.fadeOut(function() {
-                this.remove();
-            });
-        } else {
-            addBeforeElem.before("<li>" + newTaskTextField.val() + "</li>");
-            newTaskTextField.remove();
+        newTaskTextFieldFinished();
+        return;
+    });
+
+    //What to do on enter key selected
+    newTaskTextField.keypress(function(e) {
+        var key = e.which;
+        if(key === 13) {
+            newTaskTextFieldFinished();
+            newTaskTextField.off();
         }
     });
 }
